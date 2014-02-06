288d1be02eb12a5149c5783fe7a014779f7a3b9d
semmin <sbagreev@gmail.com>
2014-02-06 14:19:01 -0500

    Basic detail page

--- Resources/iphone/alloy/controllers/index.js
+++ Resources/iphone/alloy/controllers/index.js
@@ -1,4 +1,7 @@
 function Controller() {
+    function rowClick(event) {
+        Alloy.createController("detailPage", parsedData[event.index]).getView().open();
+    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "index";
     arguments[0] ? arguments[0]["__parentSymbol"] : null;
@@ -6,27 +9,44 @@ function Controller() {
     arguments[0] ? arguments[0]["__itemTemplate"] : null;
     var $ = this;
     var exports = {};
+    var __defers = {};
     $.__views.home = Ti.UI.createWindow({
         backgroundColor: "white",
         id: "home"
     });
     $.__views.home && $.addTopLevelView($.__views.home);
+    $.__views.content = Ti.UI.createView({
+        top: "20dp",
+        id: "content"
+    });
+    $.__views.home.add($.__views.content);
     $.__views.jobslist = Ti.UI.createTableView({
         id: "jobslist"
     });
-    $.__views.home.add($.__views.jobslist);
+    $.__views.content.add($.__views.jobslist);
+    rowClick ? $.__views.jobslist.addEventListener("click", rowClick) : __defers["$.__views.jobslist!click!rowClick"] = true;
     exports.destroy = function() {};
     _.extend($, $.__views);
     var quasarlib = require("quasarlib");
+    var parsedData = [];
     var onSucces = function(data) {
-        var parsedData = JSON.parse(data);
         var row;
         var rows = [];
-        parsedData["job_postings"].forEach(function(job) {
+        rawData = JSON.parse(data);
+        parsedData = rawData["job_postings"];
+        parsedData.forEach(function(job) {
             var payload = {
                 title: job.title,
                 summary: job.employer_name,
-                thumb: "http://www.employmentguide.com/company_images/1076__150x60_PlaceHolderBanner.gif"
+                thumb: "http://www.employmentguide.com/company_images/1076__150x60_PlaceHolderBanner.gif",
+                id: job.id,
+                description: job.description,
+                posted_at: job.posted_at,
+                city: job.city,
+                state: job.state,
+                zip: job.zip,
+                company_id: job.company_id,
+                apply_url: job.apply_url
             };
             row = Alloy.createController("jobs.row", payload).getView();
             rows.push(row);
@@ -39,6 +59,7 @@ function Controller() {
     var dataURL = "https://quasar-9.herokuapp.com/api/v1/job_postings?auth_token=KGTTnbzgbAC1gzTaVZjs&site_of_origin=ORION";
     quasarlib.getTableData(dataURL, onSucces, onError);
     $.home.open();
+    __defers["$.__views.jobslist!click!rowClick"] && $.__views.jobslist.addEventListener("click", rowClick);
     _.extend($, exports);
 }
 
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,15 +1,25 @@
 var quasarlib=require('quasarlib');
+var parsedData=[];
 
 var onSucces=function(data){
-	var parsedData=JSON.parse(data);
 	var row;
 	var rows=[];
+	rawData=JSON.parse(data);
+	parsedData = rawData["job_postings"];
 
-	parsedData["job_postings"].forEach(function(job){
+	parsedData.forEach(function(job){
 		var payload={
 			title: job.title,
 			summary: job.employer_name,
-			thumb: "http://www.employmentguide.com/company_images/1076__150x60_PlaceHolderBanner.gif"
+			thumb: "http://www.employmentguide.com/company_images/1076__150x60_PlaceHolderBanner.gif",
+			id: job.id,
+			description: job.description,
+			posted_at: job.posted_at,
+			city: job.city,
+			state: job.state,
+			zip: job.zip,
+			company_id: job.company_id,
+			apply_url: job.apply_url
 		};
 
 		row=Alloy.createController('jobs.row',payload).getView();
@@ -20,6 +30,10 @@ var onSucces=function(data){
 
 var onError=function(error){
 	console.log(error);
+}
+
+function rowClick(event){
+	Alloy.createController('detailPage', parsedData[event.index]).getView().open();
 }
 
 var dataURL="https://quasar-9.herokuapp.com/api/v1/job_postings?auth_token=KGTTnbzgbAC1gzTaVZjs&site_of_origin=ORION";
