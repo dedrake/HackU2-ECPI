8d140daf5899780efb69a2bf2e57ab86e5ee9305
togbaliberty <togbaliberty@homes.com>
2014-02-07 10:14:29 -0500

    Forms Page Update

--- Resources/iphone/alloy/controllers/detailPage.js
+++ Resources/iphone/alloy/controllers/detailPage.js
@@ -16,7 +16,7 @@ function Controller() {
     var exports = {};
     var __defers = {};
     $.__views.detailPage = Ti.UI.createWindow({
-        backgroundColor: "white",
+        backgroundColor: "8cacb7",
         top: "20dp",
         id: "detailPage"
     });
@@ -43,11 +43,18 @@ function Controller() {
     $.__views.scrollView = Ti.UI.createScrollView({
         top: "44dp",
         width: "320dp",
+<<<<<<< HEAD
         backgroundColor: "white",
         font: {
             fontFamily: "arial",
             fontSize: "16dp",
             fontColor: "04212b"
+=======
+        backgroundColor: "#8cacb7",
+        font: {
+            fontFamily: "Arial",
+            fontSize: "16dp"
+>>>>>>> Forms Page Update
         },
         id: "scrollView"
     });
